f86feee4b1ed5978d9e381eb917917679e1a3099
Audrius <asimpukas@gmail.com>
2021-08-22 15:06:34 +0300

    Added components html and css

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/pages/home/home.component';
+import { MaintenanceComponent } from './components/pages/maintenance/maintenance.component';
+import { ProductsComponent } from './components/pages/products/products.component';
+import { ServicemanComponent } from './components/pages/serviceman/serviceman.component';
 
 const appRoutes: Routes = [
-  {path: '', component: HomeComponent}
+  {path: '', component: HomeComponent},
+  {path: 'products', component: ProductsComponent},
+  {path: 'maintenance', component: MaintenanceComponent},
+  {path: 'service', component: ServicemanComponent}
 ]
 
 @NgModule({
